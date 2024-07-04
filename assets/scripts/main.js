cb0f64136920d0819d2721f9a9e431b32434def0
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-07-04 02:08:24 -0300

    revert: last 4 commits

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,26 +3,24 @@
   qual sessão está sendo visualizada
 */
 
-sections = document.querySelectorAll("section[id]");
+sections = document.querySelectorAll('section[id]')
 
-window.addEventListener("scroll", (scrollActive) => {
-  const scroll_y_offset = window.pageYOffset;
+window.addEventListener('scroll', scrollActive => {
+  const scroll_y_offset = window.pageYOffset
 
-  sections.forEach((current) => {
+  sections.forEach(current => {
     const section_top_offset = current.offsetTop;
     const section_height_offset = current.offsetHeight;
 
-    section_id = current.getAttribute("id");
+    section_id = current.getAttribute('id')
 
-    if (
-      scroll_y_offset > section_top_offset &&
-      scroll_y_offset <= section_top_offset + section_height_offset
-    ) {
+    if (scroll_y_offset > section_top_offset && scroll_y_offset <= section_top_offset + section_height_offset) {
       // document.querySelector('.nav_item a[href*=' + section_id + ']').classList.add('active-session')
-      document.querySelector(".nav_item").classList.add("active-session");
+      document.querySelector('.nav_item').classList.add('active-session')
     } else {
       // document.querySelector('.nav_item a[href*=' + section_id + ']').classList.remove('active-session')
-      document.querySelector(".nav_item").classList.remove("active-session");
+      document.querySelector('.nav_item').classList.remove('active-session')
     }
-  });
-});
+  }
+  )
+})
